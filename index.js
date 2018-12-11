e67d704cda6f689b98e0dee50f669e17be9b6ffe
faraniarijaona <ffaraniarijaona@gmail.com>
2018-12-11 22:28:20 +0300

    checking webhook event2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
-var webhook_controller = require('./controllers/webhookController');
-require('dotenv').config();
+let webhook_controller = require('./controllers/webhookController');
 
 const 
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json()),
-    PAGE_ACCESS_TOKEN = process.env.PAGE_ID;
+    PAGE_ACCESS_TOKEN = "EAAgXXSZAMUjkBABd4XKZAsGAgzlrPYKKMDeMo1wl1HVyDMweSiErA4sVzRFmtVnHj7kfmUPfTYcumHDRVEaV3MXLeJcHnq6MwIiY32w0rCgMT6HK7CxVpjcOh3hLYN3jf152WiFHBE6cQhCjGsG9SZBydTWIKYEwc6fZCW2ZAIAZDZD";
 
 app.get('/webhook', webhook_controller.webhook);
 app.post('/webhook', webhook_controller.webhookPost);
