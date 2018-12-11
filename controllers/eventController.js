d693b124ed5cdd7cf1163bbdb7608376289db846
faraniarijaona <ffaraniarijaona@gmail.com>
2018-12-11 23:08:57 +0300

    checking webhook event2

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -19,5 +19,24 @@ exports.postback = function(sender_psid, received_message){
 };
 
 exports.sendMessage = function(sender_psid, response){
-
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
+    
+      // Send the HTTP request to the Messenger Platform
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
 };
