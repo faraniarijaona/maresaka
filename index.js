872674858e3178df112236f5ddaa7cae6d2cfe54
faraniarijaona <ffaraniarijaona@gmail.com>
2018-12-11 22:10:34 +0300

    checking webhook event2

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var webhook_controller = require('./controllers/webhookController');
 const 
     express = require('express'),
     bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json());
+    app = express().use(bodyParser.json()),
+    PAGE_ACCESS_TOKEN = process.env.PAGE_ID;
 
 app.get('/webhook', webhook_controller.webhook);
 app.post('/webhook', webhook_controller.webhookPost);
 //app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-app.listen(process.env.PORT || 443, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 443, () => {
+    console.log('webhook is listening');
+    console.log(PAGE_ACCESS_TOKEN);
+});
