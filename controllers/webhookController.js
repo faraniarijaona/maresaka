d4eca6d665973b988bdafedca337f915dc2ebcc8
faraniarijaona <ffaraniarijaona@gmail.com>
2018-12-11 19:39:17 +0300

    init commit

---
+++ controllers/webhookController.js
@@ -0,0 +1,42 @@
+exports.webhook = function(request, response){
+    let VERIFY_TOKEN = "2081c182-fc9c-11e8-8eb2-f2801f1b9fd1";
+    let mode = request.query['hub.mode'];
+    let token = request.query['hub.verify_token'];
+    let challenge = request.query['hub.challenge'];
+
+
+    if (mode && token) {
+  
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+          
+          // Responds with the challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          response.status(200).send(challenge);
+        
+        } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          response.sendStatus(403);      
+        }
+    }
+};
+
+exports.webhookPost = function(request, response){
+    var params = request.body;
+
+    if(params.object && params.entry){
+        console.log("EVENT RECEIVED");
+        
+        (params.entry).forEach(element => {
+            (element.messaging).forEach(el=>{
+                response.send(el.message);
+            });
+        });
+    }
+    else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        response.sendStatus(403);      
+      }
+}
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+var webhook_controller = require('./controllers/webhookController');
+
+const 
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json());
+
+app.get('/webhook', webhook_controller.webhook);
+app.post('/webhook', webhook_controller.webhookPost);
+//app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(1337, () => console.log('webhook is listening'));
