4550af6f22c176d2fcb9f6e24353747d1da4c64a
faraniarijaona <ffaraniarijaona@gmail.com>
2018-12-19 08:11:52 +0300

    crontask v8

--- template/messageTemplate.js
+++ template/messageTemplate.js
@@ -1,11 +1,35 @@
 'use strict';
 
 exports.greeting = function () {
-    return {
-        "dynamic_text": {
-            "text": "Bienvenue sur Maresaka {{first_name}}!",
-            "fallback_text": "Bienvenue sur Maresaka!"
+    let lang = '{{locale}}';
+
+    if (lang.includes('mg')) {
+        return {
+            "dynamic_text": {
+                "text": "Tongasoa {{first_name}}!",
+                "fallback_text": "Tongasoa Namana!"
+            }
+        }
+    }
+    else if (lang.includes('fr')) {
+        return {
+            "dynamic_text": {
+                "text": "Bienvenue {{first_name}}!",
+                "fallback_text": "Bienvenue Ami!"
+            }
+        }
+    }
+    else {
+        return {
+            "dynamic_text": {
+                "text": "Welcome {{first_name}}!",
+                "fallback_text": "Welcome Friend!"
+            }
         }
     }
 };
+
+exports.whoisme = function () {
+
+}
 
