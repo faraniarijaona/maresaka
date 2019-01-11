d3886152cdbb5267f4ff33229ecf4e2c4139524b
faraniarijaona <ffaraniarijaona@gmail.com>
2019-01-11 07:13:38 +0300

    for chatfuel use

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -94,7 +94,134 @@ exports.latestnews = function (request, response) {
 
         console.log(JSON.stringify(mesazy));
 
-    response.send(mesazy);
+    response.send({
+        "messages": [
+          {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [
+                  {
+                    "subtitle": "https://www.banky-foibe.mg, 2019-01-11 06:07",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "subtitle": "https://www.banky-foibe.mg, 2019-01-11 06:07",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/13B99/production/_105139708_hi051558990.jpg",
+                    "title": "Trump visits border amid US shutdown wall row",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 02:52",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-us-canada-46827555",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/172A3/production/_105138849_abe.jpg",
+                    "title": "Brexit: Japan's PM says 'wish of whole world' to avoid no-deal",
+                    "subtitle": "www.bbc.co.uk, 2019-01-10 23:41",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/uk-politics-46826345",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/181E9/production/_105139789_rapper.jpg",
+                    "title": "Kevin Fret: Gay rapper shot dead in Puerto Rico aged 24",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 04:34",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-us-canada-46833286",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/0C9F/production/_103113230_048775815-1.jpg",
+                    "title": "Jakiw Palij: Nazi guard deported by US dies in Germany",
+                    "subtitle": "www.bbc.co.uk, 2019-01-10 23:47",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-europe-46828006",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/14054/production/_105140028_gettyimages-461754440.jpg",
+                    "title": "Ex-soap star charged with assault",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 05:10",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-australia-46833427",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/B665/production/_105139664_p06xs4dp.jpg",
+                    "title": "The refuge for children with microcephaly",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 03:01",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-africa-46795387",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/15B01/production/_105133888_p06xplly.jpg",
+                    "title": "Manila's 'trolley boys'",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 02:24",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-asia-46828430",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  },
+                  {
+                    "image_url": "http://c.files.bbci.co.uk/14819/production/_105139938_p06xs6dm.jpg",
+                    "title": "So why HAS the US government shut down?",
+                    "subtitle": "www.bbc.co.uk, 2019-01-11 03:48",
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://www.bbc.co.uk/news/world-us-canada-46815798",
+                        "title": "VOIR L'ARTICLE"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      });
 
 
      /*   eventController.sendMessage(messenger_id, mesazy).then(success => {
