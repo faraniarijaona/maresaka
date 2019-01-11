565b631fb53922a25433eec8081862258ba81aa0
faraniarijaona <ffaraniarijaona@gmail.com>
2019-01-11 07:30:41 +0300

    for chatfuel use

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -95,36 +95,48 @@ exports.latestnews = function (request, response) {
 
      response.send({
         "messages": [
-          {
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "generic",
-                "elements": [
-                  {
-                    "subtitle": "https://www.banky-foibe.mg, 2019-01-11 06:07",
-                    "buttons": [
-                      {
-                        "type": "web_url",
-                        "title": "VOIR L'ARTICLE"
-                      }
-                    ]
-                  },
-                  {
-                    "subtitle": "https://www.banky-foibe.mg, 2019-01-11 06:07",
-                    "buttons": [
-                      {
-                        "type": "web_url",
-                        "title": "VOIR L'ARTICLE"
-                      }
-                    ]
-                  }
-                ]
-              }
-            }
-          }
-        ]
-      });
+           {
+             "attachment":{
+               "type":"template",
+               "payload":{
+                 "template_type":"list",
+                 "top_element_style":"large",
+                 "elements":[
+                   {
+                     "title":"Chatfuel Rockets Jersey",
+                     "image_url":"http://rockets.chatfuel.com/assets/shirt.jpg",
+                     "subtitle":"Size: M",
+                     "buttons":[
+                       {
+                         "type":"web_url",
+                         "url":"https://rockets.chatfuel.com/store",
+                         "title":"View Item"
+                       }
+                     ]
+                   },
+                   {
+                     "title":"Chatfuel Rockets Jersey",
+                     "image_url":"http://rockets.chatfuel.com/assets/shirt.jpg",
+                     "subtitle":"Size: L",
+                     "default_action": {
+                       "type": "web_url",
+                       "url": "https://rockets.chatfuel.com/store",
+                       "messenger_extensions": true
+                     },
+                     "buttons":[
+                       {
+                         "type":"web_url",
+                         "url":"https://rockets.chatfuel.com/store",
+                         "title":"View Item"
+                       }
+                     ]
+                   }
+                 ]
+               }
+             }
+           }
+         ]
+       });
 
 
      /*   eventController.sendMessage(messenger_id, mesazy).then(success => {
