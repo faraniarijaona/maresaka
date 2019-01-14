93690bedf5f0cdc0c817b5c4ba0fb22587efa56c
faraniarijaona <ffaraniarijaona@gmail.com>
2019-01-14 21:15:15 +0300

    adding share button

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -93,49 +93,6 @@ exports.latestnews = function (request, response) {
                         ]
                     }
                 }
-            },
-            {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "image_aspect_ratio": "square",
-                        "elements": [
-                            {
-                                "title": "Chatfuel Rockets Jersey",
-                                "image_url": "https://rockets.chatfuel.com/assets/shirt.jpg",
-                                "subtitle": "Size: M",
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://rockets.chatfuel.com/store",
-                                        "title": "View Item"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Chatfuel Rockets Jersey",
-                                "image_url": "https://rockets.chatfuel.com/assets/shirt.jpg",
-                                "subtitle": "Size: L",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://rockets.chatfuel.com/store",
-                                    "messenger_extensions": true
-                                },
-                                "buttons": [
-                                    {
-                                        "type": "web_url",
-                                        "url": "https://rockets.chatfuel.com/store",
-                                        "title": "View Item"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-            },
-            {
-                "text": "Welcome to the Chatfuel Rockets!"
             }
         ]
     }
