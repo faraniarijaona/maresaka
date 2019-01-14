ef75309b95a21eff56ca42e9d2c902cdd8677df5
faraniarijaona <ffaraniarijaona@gmail.com>
2019-01-14 20:04:35 +0300

    adding share button

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -57,6 +57,50 @@ exports.latestnews = function (request, response) {
             "messages": [
                 {
                     "text": "Nothing special to say"
+                },
+                {
+                    "messages": [
+                        {
+                            "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "generic",
+                                    "image_aspect_ratio": "square",
+                                    "elements": [
+                                        {
+                                            "title": "Chatfuel Rockets Jersey",
+                                            "image_url": "https://rockets.chatfuel.com/assets/shirt.jpg",
+                                            "subtitle": "Size: M",
+                                            "buttons": [
+                                                {
+                                                    "type": "web_url",
+                                                    "url": "https://rockets.chatfuel.com/store",
+                                                    "title": "View Item"
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "title": "Chatfuel Rockets Jersey",
+                                            "image_url": "https://rockets.chatfuel.com/assets/shirt.jpg",
+                                            "subtitle": "Size: L",
+                                            "default_action": {
+                                                "type": "web_url",
+                                                "url": "https://rockets.chatfuel.com/store",
+                                                "messenger_extensions": true
+                                            },
+                                            "buttons": [
+                                                {
+                                                    "type": "web_url",
+                                                    "url": "https://rockets.chatfuel.com/store",
+                                                    "title": "View Item"
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    ]
                 }
             ]
         }
