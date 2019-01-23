453f1cbe09f3fe98629bd889f24c74a97419bcf2
faraniarijaona <ffaraniarijaona@gmail.com>
2019-01-23 22:59:36 +0300

    remaining

--- feedsource/feedsource.js
+++ feedsource/feedsource.js
@@ -4,7 +4,7 @@ exports.getSource = function (topic) {
     if (topic) {
 
     } else {
-        return ['http://feeds.bbci.co.uk/news/world/rss.xml', 'http://www.rfi.fr/general/rss', 'http://matv.mg/feed/', 'https://www.jeuneafrique.com/pays/madagascar/feed/',
+        return ['http://feeds.bbci.co.uk/news/world/rss.xml', 'http://www.rfi.fr/general/rss', 'http://matv.mg/feed/',
             'http://feeds.feedburner.com/MadagascarTribune'
         ];
     }
