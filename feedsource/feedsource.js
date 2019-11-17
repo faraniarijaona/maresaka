84d308f4f6620e20d01c1d22ebaabf01a81847f3
faraniarijaona <ffaraniarijaona@gmail.com>
2019-11-17 21:33:31 +0300

    update page access token

--- feedsource/feedsource.js
+++ feedsource/feedsource.js
@@ -4,8 +4,7 @@ exports.getSource = function (topic) {
     if (topic) {
 
     } else {
-        return ['http://feeds.bbci.co.uk/news/world/rss.xml', 'http://www.rfi.fr/general/rss', 'http://matv.mg/feed/',
-            'http://feeds.feedburner.com/MadagascarTribune'
+        return ['http://feeds.bbci.co.uk/news/world/rss.xml', 'http://www.rfi.fr/general/rss'
         ];
     }
 }
